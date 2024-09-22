2e1fc34c5a77ead46131e3f6d8caa8c61cfbb04c
kerry <48721345+homeautomaton@users.noreply.github.com>
2024-09-21 20:05:50 -0500

    some documentation and changed "alert" to "action" throughout

--- power-outages/power-outages.js
+++ power-outages/power-outages.js
@@ -1,22 +1,27 @@
-/************************   settings  ************************/
+/******************* poweroutages.js ***************************
+This script can monitor devices or web sites by loading an http 
+request, and if they becom unreachable, take a series of actions.
+Each action can be an MQTT message or an http web request.
+*************************   settings  ************************/
+
 notify = [ { "name": "web-a", "url":"http://192.168.1.188/rpc/switch.Toggle?id=0" },
            { "name": "web-b", "url":"http://192.168.1.189/rpc/switch.Toggle?id=0" },
            { "name": "mq-c", "topic":"updown", "message":"{device} is {state}"} ];
 
 devices = [ { "name": "plug-a",
-               "alerts": [ {"notify": "web-a", "dir": "down"}, 
+               "actions": [ {"notify": "web-a", "dir": "down"}, 
                            {"notify": "mq-c", "dir": "down"} ], 
                "url": "http://192.168.1.180/rpc/sys.getStatus",
                "poll_time": 10  },
             { "name": "plug-b",
               "enable": false,
-              "alerts": [ {"notify": "web-b", "dir": "both"} ], 
+              "actions": [ {"notify": "web-b", "dir": "both"} ], 
                "url": "http://192.168.1.181/rpc/sys.getStatus",
                "poll_time": 10  } ];
 
 cycle_time = 5;    // the script runs periodically to check if it is time to poll or send queued notifications
 // cycle time defines the minimum poll_time for any device, and affects latency of message delivery
-verbose = 3;       // level 0=quiet, 1=state changes and alerts, 2=polling, 3=copious
+verbose = 3;       // level 0=quiet, 1=state changes and actions, 2=polling, 3=copious
 
 /***************   program variables, do not change  ***************/
 in_flight = 0;
@@ -41,8 +46,8 @@ function poll_response( result, error_code, error_message, dev ) {
     }
 }
 
-function alert_response( result, error_code, error_message, notify ) {
-    if ( verbose > 2 ) print( "alert response" );
+function action_response( result, error_code, error_message, notify ) {
+    if ( verbose > 2 ) print( "action response" );
     in_flight--;
     if ( error_code != 0 )
         print( "failed to send notification: " + notify + " [" + in_flight + "]" )
@@ -54,27 +59,27 @@ function apply_templates( s, d ){
     return s;
 }
 
-function alert( d ) {
-    if ( verbose > 2 ) print( "alert" );
-    while( in_flight < 3 && d.alerts_processed < d.alerts.length ) {
-        let alert = d.alerts[ d.alerts_processed ];
+function action( d ) {
+    if ( verbose > 2 ) print( "action" );
+    while( in_flight < 3 && d.actions_processed < d.actions.length ) {
+        let action = d.actions[ d.actions_processed ];
 
-        if ( alert.dir == 'both' || alert.dir == d.state ) {
-            if ( def( notify_map[ alert.notify ].url ) ) {
+        if ( action.dir == 'both' || action.dir == d.state ) {
+            if ( def( notify_map[ action.notify ].url ) ) {
                 in_flight++;
-                let url = apply_templates( notify_map[ alert.notify ].url, d );
-                if ( verbose > 0 ) print( "webhook " + alert.notify );
-                Shelly.call( "HTTP.GET", { url: url }, alert_response,  alert.notify  );
-            } else if ( def( notify_map[ alert.notify ].topic ) && MQTT.isConnected() ) {
-                let topic = apply_templates( notify_map[ alert.notify ].topic, d );
-                let message = apply_templates( notify_map[ alert.notify ].message, d );
-                if ( verbose > 0 ) print( "MQTT " + alert.notify );
+                let url = apply_templates( notify_map[ action.notify ].url, d );
+                if ( verbose > 0 ) print( "webhook " + action.notify );
+                Shelly.call( "HTTP.GET", { url: url }, action_response,  action.notify  );
+            } else if ( def( notify_map[ action.notify ].topic ) && MQTT.isConnected() ) {
+                let topic = apply_templates( notify_map[ action.notify ].topic, d );
+                let message = apply_templates( notify_map[ action.notify ].message, d );
+                if ( verbose > 0 ) print( "MQTT " + action.notify );
                 MQTT.publish( topic, message );
             }
         }
-        d.alerts_processed ++;
+        d.actions_processed ++;
    }
-   if ( d.alerts_processed == d.alerts.length ) d.action = 'notified';
+   if ( d.actions_processed == d.actions.length ) d.action = 'notified';
 }
 
 function check_states( ) {
@@ -88,12 +93,12 @@ function check_states( ) {
             if ( ( d.state == 'unknown' || d.last_poll < now - d.poll_time ) && d.action != 'in-flight' && d.action != 'changed' ) {
                 d.action = 'in-flight';
                 in_flight++;
-                d.alerts_processed = 0;
+                d.actions_processed = 0;
                 d.last_poll = now;
                 if ( verbose > 1 ) print( "polling " + d.name + " [" + in_flight + "]" );
                 Shelly.call( "HTTP.GET", { url: d.url }, poll_response, next_device );
             } else if ( d.action == 'changed' && ( d.state == 'up' || d.state == 'down' ) ) {
-                alert( d );
+                action( d );
             }
         } else
             if ( verbose > 2 ) print( d.name + " is disabled [" + in_flight + "]" );
@@ -110,7 +115,7 @@ function init( ) {
         devices[ d ].state = 'unknown';
         devices[ d ].action = '';
         devices[ d ].last_poll = 0;
-        devices[ d ].alerts_processed = 0;
+        devices[ d ].actions_processed = 0;
         if ( ! def( devices[ d ].enable ) ) devices[ d ].enable = true
     }
     for ( let n in notify ) {
